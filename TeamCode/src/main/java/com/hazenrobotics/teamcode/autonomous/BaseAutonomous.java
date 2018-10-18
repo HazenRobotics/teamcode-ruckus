2dd5a06426266c86698493b1bf93b0266d227d38
Andy <andynguyen2171@gmail.com>
2018-10-18 16:16:54 -0700

    Worked on Step 1 and 2
    Changes:
    move to mineral
    move to wall
    kncking mineral
    move to depot
    
    TODO:
    Drop Marker
    Backing up
    Fill in part 1 move values

--- TeamCode/src/main/java/com/hazenrobotics/teamcode/autonomous/BaseAutonomous.java
+++ TeamCode/src/main/java/com/hazenrobotics/teamcode/autonomous/BaseAutonomous.java
@@ -6,6 +6,10 @@ import static com.hazenrobotics.commoncode.models.angles.UnnormalizedAngleUnit.*
 import static com.hazenrobotics.commoncode.models.angles.directions.RotationDirection.*;
 
 import static com.hazenrobotics.commoncode.models.angles.directions.SimpleDirection.*;
+
+import com.hazenrobotics.commoncode.models.colors.NamedColorList;
+import com.hazenrobotics.commoncode.models.colors.SensorColor;
+import com.hazenrobotics.commoncode.models.conditions.Condition;
 import com.hazenrobotics.commoncode.models.conditions.GyroAngle;
 import com.hazenrobotics.commoncode.models.conditions.RangeDistance;
 import com.hazenrobotics.commoncode.models.distances.Distance;
@@ -55,24 +59,52 @@ public abstract class BaseAutonomous extends LinearOpMode implements OpModeInter
         //TODO: Drop down code
         wheels.move(new Distance(10, INCH),BACKWARDS);
         wheels.turn(new Angle(135, DEGREES),COUNTER_CLOCKWISE);
-
+        //wheels.move(new Distance());
+        //wheels.turn(new Angle());
+      ;
 
 
     }
 
     protected void step2() {
         ///.....
+        //
 
         wheels.move(new RangeDistance(new Distance(10, INCH), rangeSensorFront, true), FORWARDS);
+
+        wheels.move(new Condition() {
+            NamedColorList colorList = getColorList();
+            private NamedColorList getColorList() {
+                NamedColorList list = new NamedColorList();
+                list.addColor(SensorColor.LIME);
+                list.addColor(SensorColor.LEMON);
+                list.addColor(SensorColor.YELLOW);
+                list.addColor(SensorColor.ORANGE);
+                return list;
+            }
+
+            @Override
+            protected boolean condition() {
+                return colorList.contains(colorSensorSide.getColor());
+            }
+        }, FORWARDS);
+        wheels.turn(new Angle(360, DEGREES), CLOCKWISE);
+        //
+        wheels.move(new RangeDistance(new Distance(0, INCH), rangeSensorFront, false), FORWARDS);
         wheels.turn(new GyroAngle(angleToDepot, gyroSensor, CLOCKWISE), CLOCKWISE);
     }
 
     protected void step3 () {
+        wheels.move(new RangeDistance(new Distance(24, INCH),rangeSensorFront, false), FORWARDS);
+        //TODO: Claim depot
 
     }
 
     protected void setupHardware() {
-        wheels = new TwoWheels(this, "leftMotor", "rightMotor");
+        wheels = new TwoEncoderWheels(this, "leftMotor", "rightMotor",
+                new TwoEncoderWheels.EncoderConfiguration(1680, new Distance(37f, MM),
+                        new Distance(37.3f, CM)));
+
 
     }
 
