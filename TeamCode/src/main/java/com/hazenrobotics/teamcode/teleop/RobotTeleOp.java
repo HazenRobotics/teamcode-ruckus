ba04242e6673566faf57b5a6bfa73214f4f98323
Zenos5 <angeljw@outlook.com>
2018-11-20 14:30:50 -0800

    Teleop with the sole intention of moving the parts of the arm.

--- TeamCode/src/main/java/com/hazenrobotics/teamcode/teleop/RobotTeleOp.java
+++ TeamCode/src/main/java/com/hazenrobotics/teamcode/teleop/RobotTeleOp.java
@@ -2,9 +2,6 @@ package com.hazenrobotics.teamcode.teleop;
 
 import com.hazenrobotics.commoncode.input.ButtonManager;
 import com.hazenrobotics.commoncode.interfaces.OpModeInterface;
-import com.hazenrobotics.commoncode.movement.DrivingController;
-import com.hazenrobotics.commoncode.movement.TankControlsDrivingController;
-import com.hazenrobotics.commoncode.movement.TwoWheels;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -19,38 +16,94 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 @TeleOp(name="TeleOp", group="TeleOp")
 @Disabled
 public class RobotTeleOp extends LinearOpMode implements OpModeInterface {
-    //Add all global objects and lists
-    protected ButtonManager buttons = new ButtonManager();
+    //Motors
+    protected DcMotor armMotor;
+    protected DcMotor hingeMotor;
+    protected DcMotor axelMotor;
+    protected DcMotor sweeperMotor;
+    //Constants
+    protected static final double SPEED = 0.5;
 
-    //Add Motors, Servos, Sensors, etc here
-    protected TwoWheels wheels;
-    protected DrivingController driving;
-
-
-    //Add all Constants here
-    //EX: protected final double MOTOR_POWER = 0.5;
     @Override
     public void runOpMode() {
         setupHardware();
-        setupButtons();
         //Add any further initialization (methods) here
 
         waitForStart();
 
         while (opModeIsActive()) {
-            buttons.update();
-
-
-
-            telemetry.update();
+            Arm();
+            Hinge();
+            Axel();
+            Sweeper();
             idle();
         }
-    }
-    protected void setupHardware() {
+
+        armMotor.setPower(0);
+        hingeMotor.setPower(0);
+        axelMotor.setPower(0);
+        sweeperMotor.setPower(0);
     }
 
-    protected void setupButtons() {
-        buttons = new ButtonManager();
+    protected void setupHardware() {
+        //initializes motor variables
+        armMotor = getMotor("armMotor");
+        hingeMotor = getMotor("hingeMotor");
+        axelMotor = getMotor("axelMotor");
+        sweeperMotor = getMotor("sweeperMotor");
+        armMotor.setDirection(DcMotor.Direction.FORWARD);
+        hingeMotor.setDirection(DcMotor.Direction.FORWARD);
+        axelMotor.setDirection(DcMotor.Direction.FORWARD);
+        sweeperMotor.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    //Method to extend and retract arm. Uses triggers.
+    protected void Arm(){
+        //Right trigger extends, left trigger retracts.
+        if(gamepad2.right_trigger>=0.05){
+            armMotor.setPower(gamepad2.right_trigger*SPEED);
+        }else if(gamepad2.left_trigger>=0.05){
+            armMotor.setPower(-gamepad2.left_trigger*SPEED);
+        }else{
+            armMotor.setPower(0);
+        }
+    }
+
+    //Method to pivot arm. Uses y and a buttons.
+    protected void Hinge(){
+        //Y lifts arm up, A lowers arm down.
+        if(gamepad2.y){
+            hingeMotor.setPower(SPEED);
+        }else if(gamepad2.a){
+            hingeMotor.setPower(-SPEED);
+        }else{
+            hingeMotor.setPower(0);
+        }
+    }
+
+    //Method to pivot bucket. Uses b and x buttons.
+    protected void Axel(){
+        //B lifts bucket up, X lowers bucket down.
+        if(gamepad2.b){
+            axelMotor.setPower(SPEED);
+        }else if(gamepad2.x){
+            axelMotor.setPower(-SPEED);
+        }else{
+            axelMotor.setPower(0);
+        }
+    }
+
+
+    //Method to rotate sweeper. Uses bumpers.
+    protected void Sweeper(){
+        //Right bumper sweeps things into scoop, left bumper spits stuff out.
+        if(gamepad2.right_bumper){
+            sweeperMotor.setPower(-SPEED);
+        }else if(gamepad2.left_bumper){
+            sweeperMotor.setPower(SPEED);
+        }else{
+            sweeperMotor.setPower(0);
+        }
     }
 
     @Override
