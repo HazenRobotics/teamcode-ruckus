fccae6e02cfbd1828ab54b7b250b7281f9c50679
Zenos5 <angeljw@outlook.com>
2018-12-04 15:40:38 -0800

    Changed arm functions from methods to two seperate classes to implement in the teleop function.

--- TeamCode/src/main/java/com/hazenrobotics/teamcode/teleop/RobotTeleOp.java
+++ TeamCode/src/main/java/com/hazenrobotics/teamcode/teleop/RobotTeleOp.java
@@ -6,6 +6,8 @@ import com.hazenrobotics.commoncode.movement.TankControlsDrivingController;
 import com.hazenrobotics.commoncode.movement.TwoWheels;
 import com.hazenrobotics.teamcode.DualPulleyLift;
 import com.hazenrobotics.teamcode.DualPulleyLiftController;
+import com.hazenrobotics.teamcode.ExtendyArm;
+import com.hazenrobotics.teamcode.ExtendyArmController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -26,16 +28,9 @@ public class RobotTeleOp extends LinearOpMode implements OpModeInterface {
     protected DrivingController driving;
     protected DualPulleyLift lift;
     protected DualPulleyLiftController liftController;
+    protected ExtendyArm arm;
+    protected ExtendyArmController armController;
 
-    //Motors
-    protected DcMotor armMotor;
-    protected DcMotor hingeMotor;
-    protected DcMotor axelMotor;
-    protected DcMotor sweeperMotor;
-    //Constants
-    protected static final double PIVOT_SPEED = 0.3;
-    protected static final double ARM_EXTENDING_SPEED = 0.5;
-    protected static final double SWEEPER_SPEED = 0.2;
 
     @Override
     public void runOpMode() {
@@ -53,19 +48,12 @@ public class RobotTeleOp extends LinearOpMode implements OpModeInterface {
             driving.updateMotion();
 
             liftController.updateMotion();
+            armController.updateMotion();
 
-            Arm();
-            Hinge();
-            Axel();
-            Sweeper();
             idle();
         }
         liftController.stopMotion();
-
-        armMotor.setPower(0);
-        hingeMotor.setPower(0);
-        axelMotor.setPower(0);
-        sweeperMotor.setPower(0);
+        armController.stopMotion();
     }
 
     protected void setupHardware() {
@@ -76,62 +64,8 @@ public class RobotTeleOp extends LinearOpMode implements OpModeInterface {
         lift = new DualPulleyLift(this, "extendingMotor", "retractingMotor", 1.0f);
         liftController = new DualPulleyLiftController(lift, gamepad2, gamepad1, 1f);
 
-        armMotor = getMotor("armMotor");
-        hingeMotor = getMotor("hingeMotor");
-        axelMotor = getMotor("axelMotor");
-        sweeperMotor = getMotor("sweeperMotor");
-        armMotor.setDirection(DcMotor.Direction.FORWARD);
-        hingeMotor.setDirection(DcMotor.Direction.FORWARD);
-        axelMotor.setDirection(DcMotor.Direction.FORWARD);
-        sweeperMotor.setDirection(DcMotor.Direction.FORWARD);
-    }
-
-    //Method to extend and retract arm. Uses triggers.
-    protected void Arm(){
-        //Right trigger extends, left trigger retracts.
-        if(gamepad2.right_trigger>=0.05){
-            armMotor.setPower(gamepad2.right_trigger*ARM_EXTENDING_SPEED);
-        }else if(gamepad2.left_trigger>=0.05){
-            armMotor.setPower(-gamepad2.left_trigger*ARM_EXTENDING_SPEED);
-        }else{
-            armMotor.setPower(0);
-        }
-    }
-
-    //Method to pivot arm. Uses y and a buttons.
-    protected void Hinge(){
-        //Y lifts arm up, A lowers arm down.
-        if(gamepad2.y){
-            hingeMotor.setPower(PIVOT_SPEED);
-        }else if(gamepad2.a){
-            hingeMotor.setPower(-PIVOT_SPEED);
-        }else{
-            hingeMotor.setPower(0);
-        }
-    }
-
-    //Method to pivot bucket. Uses b and x buttons.
-    protected void Axel(){
-        //B lifts bucket up, X lowers bucket down.
-        if(gamepad2.dpad_up){
-            axelMotor.setPower(PIVOT_SPEED);
-        }else if(gamepad2.dpad_down){
-            axelMotor.setPower(-PIVOT_SPEED);
-        }else{
-            axelMotor.setPower(0);
-        }
-    }
-
-    //Method to rotate sweeper. Uses bumpers.
-    protected void Sweeper(){
-        //Right bumper sweeps things into scoop, left bumper spits stuff out.
-        if(gamepad2.right_bumper){
-            sweeperMotor.setPower(-SWEEPER_SPEED);
-        }else if(gamepad2.left_bumper){
-            sweeperMotor.setPower(SWEEPER_SPEED);
-        }else{
-            sweeperMotor.setPower(0);
-        }
+        arm = new ExtendyArm(this, "armMotor", "hingeMotor", "axelMotor", "sweeperMotor");
+        armController = new ExtendyArmController(arm, gamepad2);
     }
 
     @Override
